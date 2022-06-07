4c9abce61a8153bf462745e1ea61cd820b996bd9
Salar Dev <72287943+salar-dev@users.noreply.github.com>
2022-06-07 12:05:17 +0300

    Add files via upload

---
+++ addallposts.py
@@ -0,0 +1,55 @@
+import requests
+import json
+from tkinter import filedialog
+
+
+url = "https://instagram-media-downloader.p.rapidapi.com/rapid/post.php"
+
+postUrl = input("Enter Post Url >>> ")
+getFileName = input('Enter File Name >>> ')
+fileDec = filedialog.askdirectory()
+
+
+querystring = {"url": postUrl}
+
+headers = {
+    "x-rapidapi-host": "instagram-media-downloader.p.rapidapi.com",
+    "x-rapidapi-Key": "341fd61403msh923c5b09db48599p16e705jsndfaca83fb8b1" # Put Your API Key 
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+# print(response.text)
+
+textToJson = json.loads(response.text)
+print(textToJson)
+
+postFileUrl = ''
+
+if 'video' in textToJson:
+    postFileUrl = textToJson["video"]
+elif 'image' in textToJson:
+    postFileUrl = textToJson["image"]
+else :
+    for i in range(10):
+        postFileUrl = textToJson[str(i)]
+        reqPostFileUrl = requests.get(postFileUrl)
+        fileEx = reqPostFileUrl.headers['Content-Type'].split('/')[-1].split('.')[0]
+        fileName = fileDec + '/' + getFileName + str(i) + '.' + fileEx
+        with reqPostFileUrl as rq:
+            with open(fileName, 'wb') as file:
+                file.write(rq.content)
+        if str(i) not in textToJson:
+            break
+
+
+reqPostFileUrl = requests.get(postFileUrl)
+
+fileEx = reqPostFileUrl.headers['Content-Type'].split('/')[-1].split('.')[0]
+
+fileName = fileDec + '/' + getFileName + '.' + fileEx
+
+with reqPostFileUrl as rq:
+    with open(fileName, 'wb') as file:
+        file.write(rq.content)
+
+print(textToJson)
